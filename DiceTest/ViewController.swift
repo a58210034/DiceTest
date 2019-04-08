30b8d5090a160772145808fe02c014a131f89a94
Sinyuan <a58210034@gmail.com>
2019-04-08 15:21:34 +0800

    Initial Commit

---
+++ DiceTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiceTest
+//
+//  Created by Sin-yuan Jiang on 2019/4/8.
+//  Copyright © 2019 Sin-yuan Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
